fde0fd5f224c31a3dcc4c8e823aa3d7f79d267f4
Gregory Daynes <greg@stembolt.com>
2018-03-17 17:24:12 -0700

    Update postcss tooling
    Update SCSS parsing, add file import scheme

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
+require('postcss-scss');
+require('postcss-easy-import');
+
 const autoprefixer = require('autoprefixer');
 
-module.exports = ctx => ({
+module.exports = {
+  parser: 'postcss-scss',
   plugins: {
+    'postcss-easy-import': { extensions: '.scss', prefix: '_' },
     'postcss-node-sass': {},
     autoprefixer: { browsers: ['last 1 versions'] },
     cssnano: { preset: 'default' },
   }
-});
+}
